2a2c1ca6a1b19c0686411a3b5465b99bfa019f11
Nouman Aslam <mrnoumannomi7@gmail.com>
2026-06-21 14:08:37 +0500

    Adding Authentication using Clerk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
-
+import { useUser, SignInButton, SignOutButton } from '@clerk/react';
 const Navbar = () => {
+    const { isSignedIn, user,isLoaded } = useUser();     
   return (
     <nav className="h-16 border-b border-stone-200 bg-white px-6 md:px-12">
       <div className="mx-auto flex h-full max-w-7xl items-center justify-between">
@@ -15,10 +16,31 @@ const Navbar = () => {
           </h1>
         </div>
 
+
+
         {/* User */}
-        <button className="text-2xl text-stone-400 transition hover:text-stone-900">
-          <FaRegCircleUser />
-        </button>
+        <div className="flex items-center gap-4">
+        {!isSignedIn ? (
+          <SignInButton mode="modal">
+            <button className="flex items-center gap-2 text-2xl text-stone-400 transition hover:text-stone-900">
+              <FaRegCircleUser />
+              <span className="text-sm font-medium">Login</span>
+            </button>
+          </SignInButton>
+        ) : (
+          <div className="flex items-center gap-3">
+            <span className="text-sm font-medium text-gray-700">
+              {user?.firstName || user?.username}
+            </span>
+            <SignOutButton>
+              <button className="flex items-center gap-1 text-2xl text-stone-400 transition hover:text-stone-900">
+                <FaRegCircleUser />
+                <span className="text-xs font-medium">Logout</span>
+              </button>
+            </SignOutButton>
+          </div>
+        )}
+      </div>
       </div>
     </nav>
   );
